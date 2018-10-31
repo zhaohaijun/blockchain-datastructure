5ae787fe259f159fda5e14113487df3468b56782
zhaohaijun <eshezhao@gmail.com>
2018-10-31 14:02:59 +0800

    init

---
+++ merkle_tree.go
@@ -0,0 +1,219 @@
+package blockchaindatastructure
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+)
+
+type Content interface {
+	CalculateHash() ([]byte, error)
+	Equals(other Content) (bool, error)
+}
+type MerkleTree struct {
+	Root       *Node
+	merkleRoot []byte
+	Leafs      []*Node
+}
+type Node struct {
+	Parent *Node
+	Left   *Node
+	Right  *Node
+	leaf   bool
+	dup    bool
+	Hash   []byte
+	C      Content
+}
+
+func (n *Node) verifyNode() ([]byte, error) {
+	if n.leaf {
+		return n.C.CalculateHash()
+	}
+	rightBytes, err := n.Right.verifyNode()
+	if err != nil {
+		return nil, err
+	}
+	leftBytes, err := n.Left.verifyNode()
+	if err != nil {
+		return nil, err
+	}
+	h := sha256.New()
+	if _, err := h.Write(append(leftBytes, rightBytes...)); err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
+}
+func (n *Node) calculateNodeHash() ([]byte, error) {
+	if n.leaf {
+		return n.C.CalculateHash()
+	}
+	h := sha256.New()
+	if _, err := h.Write(append(n.Left.Hash, n.Right.Hash...)); err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
+}
+
+//NewTree creates a new Merkle Tree using the content cs.
+func NewTree(cs []Content) (*MerkleTree, error) {
+	root, leafs, err := buildWithContent(cs)
+	if err != nil {
+		return nil, err
+	}
+	t := &MerkleTree{
+		Root:       root,
+		merkleRoot: root.Hash,
+		Leafs:      leafs,
+	}
+	return t, nil
+}
+
+//buildWithContent is a helper function that for a given set of Contents, generates a
+//corresponding tree and returns the root node, a list of leaf nodes, and a possible error.
+//Returns an error if cs contains no Contents.
+func buildWithContent(cs []Content) (*Node, []*Node, error) {
+	if len(cs) == 0 {
+		return nil, nil, errors.New("error: cannot construct tree with no content")
+	}
+	var leafs []*Node
+	for _, c := range cs {
+		hash, err := c.CalculateHash()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		leafs = append(leafs, &Node{
+			Hash: hash,
+			C:    c,
+			leaf: true,
+		})
+	}
+	if len(leafs)%2 == 1 {
+		duplicate := &Node{
+			Hash: leafs[len(leafs)-1].Hash,
+			C:    leafs[len(leafs)-1].C,
+			leaf: true,
+			dup:  true,
+		}
+		leafs = append(leafs, duplicate)
+	}
+	root, err := buildIntermediate(leafs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return root, leafs, nil
+}
+func buildIntermediate(n1 []*Node) (*Node, error) {
+	var nodes []*Node
+	for i := 0; i < len(n1); i += 2 {
+		h := sha256.New()
+		var left, right int = i, i + 1
+		if i+1 == len(n1) {
+			right = i
+		}
+		chash := append(n1[left].Hash, n1[right].Hash...)
+		if _, err := h.Write(chash); err != nil {
+			return nil, err
+		}
+		n := &Node{
+			Left:  n1[left],
+			Right: n1[right],
+			Hash:  h.Sum(nil),
+		}
+		nodes = append(nodes, n)
+		n1[left].Parent = n
+		n1[right].Parent = n
+		if len(n1) == 2 {
+			return n, nil
+		}
+	}
+	return buildIntermediate(nodes)
+}
+func (m *MerkleTree) MerkleRoot() []byte {
+	return m.merkleRoot
+}
+func (m *MerkleTree) RebuildTree() error {
+	var cs []Content
+	for _, c := range m.Leafs {
+		cs = append(cs, c.C)
+	}
+	root, leafs, err := buildWithContent(cs)
+	if err != nil {
+		return err
+	}
+	m.Root = root
+	m.Leafs = leafs
+	m.merkleRoot = root.Hash
+	return nil
+}
+func (m *MerkleTree) RebuildTreeWith(cs []Content) error {
+	root, leafs, err := buildWithContent(cs)
+	if err != nil {
+		return err
+	}
+	m.Root = root
+	m.Leafs = leafs
+	m.merkleRoot = root.Hash
+	return nil
+}
+func (m *MerkleTree) VerifyTree() (bool, error) {
+	calculateMerkleRoot, err := m.Root.verifyNode()
+	if err != nil {
+		return false, err
+	}
+	if bytes.Compare(m.merkleRoot, calculateMerkleRoot) == 0 {
+		return true, nil
+	}
+	return false, nil
+}
+func (m *MerkleTree) VerifyContent(content Content) (bool, error) {
+	for _, l := range m.Leafs {
+		ok, err := l.C.Equals(content)
+		if err != nil {
+			return false, err
+		}
+		if ok {
+			currentParent := l.Parent
+			for currentParent != nil {
+				h := sha256.New()
+				rightBytes, err := currentParent.Right.calculateNodeHash()
+				if err != nil {
+					return false, err
+				}
+				leftBytes, err := currentParent.Left.calculateNodeHash()
+				if err != nil {
+					return false, err
+				}
+				if currentParent.Left.leaf && currentParent.Right.leaf {
+					if _, err := h.Write(append(leftBytes, rightBytes...)); err != nil {
+						return false, err
+					}
+					if bytes.Compare(h.Sum(nil), currentParent.Hash) != 0 {
+						return false, nil
+					}
+					currentParent = currentParent.Parent
+				} else {
+					if _, err := h.Write(append(leftBytes, rightBytes...)); err != nil {
+						return false, err
+					}
+					if bytes.Compare(h.Sum(nil), currentParent.Hash) != 0 {
+						return false, nil
+					}
+					currentParent = currentParent.Parent
+				}
+			}
+			return true, nil
+		}
+	}
+	return false, nil
+}
+func (m *MerkleTree) String() string {
+	s := ""
+	for _, l := range m.Leafs {
+		s += fmt.Sprint(l)
+		s += "\n"
+	}
+	return s
+}
